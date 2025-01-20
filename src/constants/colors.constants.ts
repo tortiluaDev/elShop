d52547df255f38503971c60db44e689b5e10743e
tortiluaDev <artemii.webdes@gmail.com>
2025-01-20 16:38:37 +0300

    project setup

---
+++ src/constants/colors.constants.ts
@@ -0,0 +1,8 @@
+export const COLORS = {
+	bg: '#f5f5f5',
+	primary: '#5452E8',
+	primaryActive: '#5E5CEA',
+	white: 'f5f5f5',
+	gray: '#818185',
+	black: '#070708',
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,22 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
+
+import { COLORS } from './src/constants/colors.constants'
 
 export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
-} satisfies Config;
+	content: ['./src/components/**/*.{js,ts,jsx,tsx,mdx}', './src/app/**/*.{js,ts,jsx,tsx,mdx}'],
+	theme: {
+		extend: {
+			colors: COLORS,
+			padding: {
+				layout: '1.2rem'
+			},
+			transitionTimingFunction: {
+				DEFAULT: 'ease-in-out'
+			},
+			transitionDuration: {
+				DEFAULT: '333ms'
+			}
+		}
+	},
+	plugins: []
+} satisfies Config
